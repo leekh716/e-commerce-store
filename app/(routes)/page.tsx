1ffccf9cf5418669d649f2928844fdf379122046
kunlee <khlee@ebrain.kr>
2023-10-11 01:25:02 +0900

    feat: store home products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,23 @@
-export default function HomePage() {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("0e39c313-9f86-4e46-a17f-2b9ecb61d458");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 }
